c3b56d6cdac7c0a9ecb7abf6eb336eadf6795974
Josh Hudnall <josh@fastpxl.com>
2015-07-11 21:55:22 -0600

    Initial Commit

---
+++ JHStandardLib/JHStandardLib.h
@@ -0,0 +1,13 @@
+//
+//  JHStandardLib.h
+//  JHStandardLib
+//
+//  Created by Josh Hudnall on 7/11/15.
+//  Copyright (c) 2015 Josh Hudnall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHStandardLib : NSObject
+
+@end
---
+++ JHStandardLib/JHStandardLib.m
@@ -0,0 +1,13 @@
+//
+//  JHStandardLib.m
+//  JHStandardLib
+//
+//  Created by Josh Hudnall on 7/11/15.
+//  Copyright (c) 2015 Josh Hudnall. All rights reserved.
+//
+
+#import "JHStandardLib.h"
+
+@implementation JHStandardLib
+
+@end
